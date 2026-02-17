3004b5b263b4e01d7c144e7f7b2a9c68cdc16901
dkorzhivina <darya_korzhevina@gmail.com>
2026-02-17 22:13:31 +0300

    коммит 2

--- frame1/Controllers/TasksController.cs
+++ frame1/Controllers/TasksController.cs
@@ -1,6 +1,40 @@
-﻿namespace frame1.Controllers
+﻿using frame1.Models;
+using frame1.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace frame1.Controllers;
+
+[ApiController]
+[Route("api/tasks")]
+public class TasksController : ControllerBase
 {
-    public class TasksController
+    private readonly ITaskService _service;
+
+    public TasksController(ITaskService service)
     {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(_service.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var item = _service.GetById(id);
+        if (item == null)
+            throw new KeyNotFoundException("Задача не найдена");
+
+        return Ok(item);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] TaskItem item)
+    {
+        var created = _service.Create(item);
+        return Ok(created);
     }
 }
